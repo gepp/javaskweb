f1d9b8e4aedc867fcde6ca58d2350253b6504fcd
jdk2010 <jdk2010@163.com>
2015-12-09 19:03:06 +0800

    TJ

--- WebContent/资料/2015-12-7.sql
+++ WebContent/资料/2015-12-7.sql
@@ -13,7 +13,7 @@ MySQL - 5.5.20 : Database - wlsk
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`wlsk` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`wlsk` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `wlsk`;
 
@@ -226,9 +226,11 @@ CREATE TABLE `skq_bdclp` (
 DROP TABLE IF EXISTS `skq_bdcmm`;
 
 CREATE TABLE `skq_bdcmm` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `NSRWJBM` varchar(20) DEFAULT NULL COMMENT '纳税人微机编码',
   `PASSWORD` varchar(32) DEFAULT NULL COMMENT '密码',
-  `CJSJ` datetime DEFAULT NULL COMMENT '创建时间'
+  `CJSJ` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `skq_bdcmm` */
@@ -282,6 +284,23 @@ CREATE TABLE `skq_bdczxm` (
 
 /*Data for the table `skq_bdczxm` */
 
+/*Table structure for table `skq_dhde` */
+
+DROP TABLE IF EXISTS `skq_dhde`;
+
+CREATE TABLE `skq_dhde` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `JQBH` varchar(16) NOT NULL COMMENT '机器编号',
+  `DZKPXE` decimal(16,2) NOT NULL COMMENT '机器编号',
+  `YLJKPXE` decimal(16,2) NOT NULL COMMENT '月累计开票限额',
+  `YLJTPXE` decimal(16,2) NOT NULL COMMENT '月累计开票限额',
+  `QYBZ` int(11) NOT NULL COMMENT '启用标示',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_dhde` */
+
 /*Table structure for table `skq_fp` */
 
 DROP TABLE IF EXISTS `skq_fp`;
@@ -298,6 +317,92 @@ CREATE TABLE `skq_fp` (
 
 insert  into `skq_fp`(`id`,`FPBM`,`FPMC`,`STATUS`) values (2,'001','发票A',1);
 
+/*Table structure for table `skq_fpj` */
+
+DROP TABLE IF EXISTS `skq_fpj`;
+
+CREATE TABLE `skq_fpj` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `FPBM` varchar(20) NOT NULL COMMENT '发票编码',
+  `FPDM` varchar(20) NOT NULL COMMENT '发票编码',
+  `FPQSH` int(11) NOT NULL COMMENT '发票起始号码',
+  `FPJZH` int(11) NOT NULL COMMENT '发票截止号码',
+  `FPDW` int(11) NOT NULL COMMENT '发票单位',
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `FPLGRQ` datetime DEFAULT NULL COMMENT '发票领购日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_fpj` */
+
+/*Table structure for table `skq_fpjmx` */
+
+DROP TABLE IF EXISTS `skq_fpjmx`;
+
+CREATE TABLE `skq_fpjmx` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `FPBM` varchar(20) NOT NULL COMMENT '发票编码',
+  `FPDM` varchar(20) NOT NULL COMMENT '发票编码',
+  `FPQSH` int(11) NOT NULL COMMENT '发票起始号',
+  `FPJZH` int(11) NOT NULL COMMENT '发票截止号',
+  `FPDW` int(11) NOT NULL COMMENT '发票单位',
+  `JQBH` varchar(16) DEFAULT NULL COMMENT '机器编号',
+  `FPXFZT` int(11) NOT NULL COMMENT '发票下发状态',
+  `FPLGZT` int(11) NOT NULL COMMENT '发票领购状态',
+  `MXSCZT` int(11) NOT NULL COMMENT '明细上传状态',
+  `FPLGRQ` datetime DEFAULT NULL COMMENT '发票领购日期',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_fpjmx` */
+
+/*Table structure for table `skq_fpkj` */
+
+DROP TABLE IF EXISTS `skq_fpkj`;
+
+CREATE TABLE `skq_fpkj` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `JQBH` varchar(16) NOT NULL COMMENT '机器编号',
+  `KPRQ` datetime DEFAULT NULL COMMENT '开票日期',
+  `KPLX` int(11) DEFAULT NULL COMMENT '开票类型',
+  `FPDM` varchar(20) NOT NULL COMMENT '发票代码',
+  `FPHM` int(11) NOT NULL COMMENT '发票号码',
+  `HJZJE` decimal(16,2) DEFAULT NULL COMMENT '开票金额',
+  `YFPHM` int(11) DEFAULT NULL COMMENT '原发票号码',
+  `FKDW` varchar(50) DEFAULT NULL COMMENT '付款单位',
+  `SKY` varchar(20) DEFAULT NULL COMMENT '收款员',
+  `XMS` int(11) DEFAULT NULL COMMENT '项目数',
+  `SKM` varchar(20) DEFAULT NULL COMMENT '税控码',
+  `QRBZ` int(11) NOT NULL COMMENT '确认标志',
+  `STATUS` int(11) DEFAULT NULL COMMENT '状态',
+  `SCSJ` datetime DEFAULT NULL COMMENT '上传时间',
+  `BZ` varchar(500) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_fpkj` */
+
+/*Table structure for table `skq_fpkjxm` */
+
+DROP TABLE IF EXISTS `skq_fpkjxm`;
+
+CREATE TABLE `skq_fpkjxm` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `PARENTID` int(11) NOT NULL COMMENT '发票开具ID',
+  `XMMC` varchar(50) NOT NULL COMMENT '项目名称',
+  `XSWS` int(11) NOT NULL COMMENT '小数位数',
+  `SL` decimal(16,2) NOT NULL COMMENT '数量',
+  `DJ` decimal(16,2) NOT NULL COMMENT '单价',
+  `JE` decimal(16,2) NOT NULL COMMENT '金额',
+  `SMBM` varchar(20) NOT NULL COMMENT '税目编码',
+  `JSXM` int(11) DEFAULT NULL COMMENT '计税项目',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_fpkjxm` */
+
 /*Table structure for table `skq_hy` */
 
 DROP TABLE IF EXISTS `skq_hy`;
@@ -309,9 +414,11 @@ CREATE TABLE `skq_hy` (
   `HYJC` varchar(50) DEFAULT NULL COMMENT '行业简称',
   `STATUS` int(11) DEFAULT NULL COMMENT '状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `skq_hy` */
+
+insert  into `skq_hy`(`id`,`HYBM`,`HYMC`,`HYJC`,`STATUS`) values (1,'001','服务业','服务业',1),(2,'001','娱乐','娱乐',1);
 
 /*Table structure for table `skq_hymx` */
 
@@ -326,9 +433,80 @@ CREATE TABLE `skq_hymx` (
   `HYMXJC` varchar(50) DEFAULT NULL COMMENT '行业明细简称',
   `STATUS` int(11) DEFAULT NULL COMMENT '状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `skq_hymx` */
+
+insert  into `skq_hymx`(`id`,`HYID`,`HYBM`,`HYMXBM`,`HYMXMC`,`HYMXJC`,`STATUS`) values (1,1,'001','0001','洗脚','洗脚',0),(4,2,'001','001','唱歌','唱歌',1);
+
+/*Table structure for table `skq_hzsj` */
+
+DROP TABLE IF EXISTS `skq_hzsj`;
+
+CREATE TABLE `skq_hzsj` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `YEAR` int(11) NOT NULL COMMENT '年',
+  `MONTH` int(11) NOT NULL COMMENT '月',
+  `ZCPFS` int(11) NOT NULL COMMENT '正常票份数',
+  `TPFS` int(11) NOT NULL COMMENT '退票份数',
+  `FPFS` int(11) NOT NULL COMMENT '废票份数',
+  `ZCPZJE` decimal(16,2) NOT NULL COMMENT '正常票总金额',
+  `TPZJE` decimal(16,2) NOT NULL COMMENT '退票总金额',
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `HZRQ` datetime DEFAULT NULL COMMENT '汇总日期',
+  `CLBZ` int(11) DEFAULT NULL COMMENT '处理标志',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_hzsj` */
+
+/*Table structure for table `skq_hzsjmx` */
+
+DROP TABLE IF EXISTS `skq_hzsjmx`;
+
+CREATE TABLE `skq_hzsjmx` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `PARENTID` int(11) NOT NULL COMMENT '申报汇总ID',
+  `SMBM` varchar(20) NOT NULL COMMENT '税目编码',
+  `KPJE` decimal(16,2) NOT NULL COMMENT '开票金额',
+  `DKJE` decimal(16,2) DEFAULT NULL COMMENT '抵扣金额',
+  `YHDYYE` decimal(16,2) DEFAULT NULL COMMENT '月核定营业额',
+  `FLAG` int(11) DEFAULT NULL COMMENT '标示',
+  `SBJE` decimal(16,2) DEFAULT NULL COMMENT '申报金额',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_hzsjmx` */
+
+/*Table structure for table `skq_jksjkz` */
+
+DROP TABLE IF EXISTS `skq_jksjkz`;
+
+CREATE TABLE `skq_jksjkz` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `TZYY` varchar(500) NOT NULL COMMENT '原因',
+  `TZSJ` datetime NOT NULL COMMENT '限制时间',
+  `CJZ` varchar(20) NOT NULL COMMENT '创建者',
+  `STATUS` int(11) NOT NULL COMMENT '状态',
+  `KFSJ` datetime DEFAULT NULL COMMENT '开放时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_jksjkz` */
+
+/*Table structure for table `skq_jqszsm` */
+
+DROP TABLE IF EXISTS `skq_jqszsm`;
+
+CREATE TABLE `skq_jqszsm` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `JQBH` varchar(16) NOT NULL COMMENT '机器编号',
+  `SMBM` varchar(20) NOT NULL COMMENT '税目编码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_jqszsm` */
 
 /*Table structure for table `skq_jqxh` */
 
@@ -346,6 +524,48 @@ CREATE TABLE `skq_jqxh` (
 /*Data for the table `skq_jqxh` */
 
 insert  into `skq_jqxh`(`id`,`JQXHBM`,`JQXHMC`,`SCCS`,`STATUS`) values (1,'DT0001','机器型号','江苏双阿基',1);
+
+/*Table structure for table `skq_jqxx` */
+
+DROP TABLE IF EXISTS `skq_jqxx`;
+
+CREATE TABLE `skq_jqxx` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `JQXHBM` varchar(20) NOT NULL COMMENT '机器型号编码',
+  `JQBH` varchar(16) NOT NULL COMMENT '机器编号',
+  `SKKH` varchar(16) NOT NULL COMMENT '税控卡号',
+  `YHKH` varchar(16) NOT NULL COMMENT '用户卡号',
+  `KQYRQ` datetime NOT NULL COMMENT '卡启用日期',
+  `KYXRQ` datetime NOT NULL COMMENT '卡有效日期',
+  `SBFS` varchar(2) NOT NULL COMMENT '申报方式',
+  `MXSBBZ` varchar(2) NOT NULL COMMENT '明细申报标志',
+  `KPJZRQ` datetime NOT NULL COMMENT '开票截止日期',
+  `DZKPXE` decimal(16,2) NOT NULL COMMENT '单张开票限额',
+  `YLJKPXE` decimal(16,2) NOT NULL COMMENT '月累计开票限额',
+  `YLJTPXE` decimal(16,2) NOT NULL COMMENT '月累计开票限额',
+  `STATUS` int(11) NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_jqxx` */
+
+/*Table structure for table `skq_jsgz` */
+
+DROP TABLE IF EXISTS `skq_jsgz`;
+
+CREATE TABLE `skq_jsgz` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `SWJGBM` varchar(8) NOT NULL COMMENT '税务机关编码',
+  `SMBM` varchar(8) NOT NULL COMMENT '税目编码',
+  `SMJC` varchar(20) DEFAULT NULL COMMENT '税目简称',
+  `TZBS` int(11) NOT NULL COMMENT '特征标示',
+  `JSGZ` varchar(200) DEFAULT NULL COMMENT '计税规则',
+  `QYBJ` int(11) DEFAULT NULL COMMENT '启用标示',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_jsgz` */
 
 /*Table structure for table `skq_jzaz` */
 
@@ -381,7 +601,7 @@ CREATE TABLE `skq_jzaz` (
 DROP TABLE IF EXISTS `skq_jzazkp`;
 
 CREATE TABLE `skq_jzazkp` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `PARENTID` int(11) DEFAULT NULL COMMENT '项目ID',
   `KXXZ` int(11) DEFAULT NULL COMMENT '款项性质',
   `FPDM` varchar(20) DEFAULT NULL COMMENT '发票代码',
@@ -391,17 +611,36 @@ CREATE TABLE `skq_jzazkp` (
   `KPRQ` datetime DEFAULT NULL COMMENT '开票日期',
   `SCRQ` datetime DEFAULT NULL COMMENT '上传日期',
   `JSXM` int(11) DEFAULT NULL,
-  `SFDK` int(11) DEFAULT NULL
+  `SFDK` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `skq_jzazkp` */
+
+/*Table structure for table `skq_nsrszsm` */
+
+DROP TABLE IF EXISTS `skq_nsrszsm`;
+
+CREATE TABLE `skq_nsrszsm` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `SZBM` varchar(20) NOT NULL COMMENT '税种编码',
+  `SMBM` varchar(20) NOT NULL COMMENT '税种编码',
+  `SMMC` varchar(100) NOT NULL COMMENT '税种编码',
+  `SMJC` varchar(50) NOT NULL COMMENT '税种编码',
+  `SL` decimal(9,2) NOT NULL COMMENT '税率',
+  `STATUS` int(11) DEFAULT NULL COMMENT '状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_nsrszsm` */
 
 /*Table structure for table `skq_nsrxx` */
 
 DROP TABLE IF EXISTS `skq_nsrxx`;
 
 CREATE TABLE `skq_nsrxx` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
   `NSRSBH` varchar(20) DEFAULT NULL COMMENT '纳税人识别号',
   `NSRMC` varchar(100) NOT NULL COMMENT '纳税人名称',
@@ -415,10 +654,33 @@ CREATE TABLE `skq_nsrxx` (
   `SWJGBM` varchar(20) NOT NULL COMMENT '税务机关编码',
   `BSY` varchar(20) DEFAULT NULL COMMENT '办税员',
   `SSGLY` varchar(20) DEFAULT NULL COMMENT '联系方式',
-  `STATUS` int(11) NOT NULL COMMENT '状态'
+  `STATUS` int(11) NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `skq_nsrxx` */
+
+/*Table structure for table `skq_pmbg` */
+
+DROP TABLE IF EXISTS `skq_pmbg`;
+
+CREATE TABLE `skq_pmbg` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `JQBH` varchar(16) NOT NULL COMMENT '机器编号',
+  `YSMBM` varchar(100) NOT NULL COMMENT '原税目编码',
+  `BGSMBM` varchar(100) NOT NULL COMMENT '变更税目编码',
+  `BGSM` varchar(200) DEFAULT NULL COMMENT '变更说明',
+  `SQSJ` datetime NOT NULL COMMENT '申请时间',
+  `CLBZ` int(11) NOT NULL COMMENT '申请时间',
+  `CLZ` varchar(20) DEFAULT NULL COMMENT '处理者',
+  `CLYJ` varchar(200) DEFAULT NULL COMMENT '处理意见',
+  `CLSJ` datetime DEFAULT NULL COMMENT '处理意见',
+  `XZBS` int(11) DEFAULT NULL COMMENT '处理意见',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_pmbg` */
 
 /*Table structure for table `skq_pmsz` */
 
@@ -443,6 +705,151 @@ CREATE TABLE `skq_pmsz` (
 /*Data for the table `skq_pmsz` */
 
 insert  into `skq_pmsz`(`id`,`SZBM`,`SMBM`,`SMMC`,`SMJC`,`SL`,`SMSY`,`FPBM`,`HDKPBL`,`CZKPXE`,`CEZS`,`STATUS`) values (3,'0001','1','1','1','1.00',1,'001',123,'123.00',121,1);
+
+/*Table structure for table `skq_sbczzshjg` */
+
+DROP TABLE IF EXISTS `skq_sbczzshjg`;
+
+CREATE TABLE `skq_sbczzshjg` (
+  `id` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `YEAR` int(11) NOT NULL COMMENT '年份',
+  `MONTH` int(11) NOT NULL COMMENT '月份',
+  `KPSK` decimal(16,2) NOT NULL COMMENT '开票税款',
+  `SBSK` decimal(16,2) NOT NULL COMMENT '申报税款',
+  `CLBZ` int(11) NOT NULL COMMENT '创建时间',
+  `CJSJ` datetime NOT NULL COMMENT '处理标示',
+  `CLSJ` datetime DEFAULT NULL COMMENT '处理时间',
+  `CLZ` varchar(20) DEFAULT NULL COMMENT '处理者',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_sbczzshjg` */
+
+/*Table structure for table `skq_sbhdhjg` */
+
+DROP TABLE IF EXISTS `skq_sbhdhjg`;
+
+CREATE TABLE `skq_sbhdhjg` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `YEAR` int(11) NOT NULL COMMENT '年份',
+  `MONTH` int(11) NOT NULL COMMENT '月份',
+  `CJSJ` datetime NOT NULL COMMENT '创建时间',
+  `CLBZ` int(11) NOT NULL COMMENT '处理标示',
+  `CLZ` varchar(20) DEFAULT NULL COMMENT '处理时间',
+  `CLSJ` datetime DEFAULT NULL COMMENT '处理者',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_sbhdhjg` */
+
+/*Table structure for table `skq_sbsj` */
+
+DROP TABLE IF EXISTS `skq_sbsj`;
+
+CREATE TABLE `skq_sbsj` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `SSKSSJ` datetime NOT NULL COMMENT '所属开始时间',
+  `SSJZSJ` datetime NOT NULL COMMENT '所属截止时间',
+  `ZCPFS` int(11) NOT NULL COMMENT '正常票份数',
+  `TPFS` int(11) NOT NULL COMMENT '退票份数',
+  `FPFS` int(11) NOT NULL COMMENT '废票份数',
+  `ZCPZJE` decimal(16,2) NOT NULL COMMENT '正常票总金额',
+  `TPZJE` decimal(16,2) NOT NULL COMMENT '退票总金额',
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `JQBH` varchar(16) NOT NULL COMMENT '机器编号',
+  `SBLX` int(11) NOT NULL COMMENT '申报类型',
+  `KPJZSJ` datetime DEFAULT NULL COMMENT '开票截止日期',
+  `DZKPXE` decimal(16,2) DEFAULT NULL COMMENT '单张开票限额',
+  `YLJKPXE` decimal(16,2) DEFAULT NULL COMMENT '月累计开票限额',
+  `YLJTPXE` decimal(16,2) DEFAULT NULL COMMENT '月累计退票限额',
+  `XZZT` int(11) DEFAULT NULL COMMENT '下载状态',
+  `XZRQ` datetime DEFAULT NULL COMMENT '下载日期',
+  `CJRQ` datetime DEFAULT NULL COMMENT '创建日期',
+  `SBRQ` datetime DEFAULT NULL COMMENT '申报日期',
+  `MAC` varchar(20) DEFAULT NULL COMMENT 'MAC',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_sbsj` */
+
+/*Table structure for table `skq_sbsjmx` */
+
+DROP TABLE IF EXISTS `skq_sbsjmx`;
+
+CREATE TABLE `skq_sbsjmx` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `PARENTID` int(11) NOT NULL COMMENT '申报数据ID',
+  `SMBM` varchar(20) NOT NULL COMMENT '税目编码',
+  `JE` decimal(16,2) NOT NULL COMMENT '金额',
+  `KPLX` int(11) NOT NULL COMMENT '开票类型',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_sbsjmx` */
+
+/*Table structure for table `skq_skjs` */
+
+DROP TABLE IF EXISTS `skq_skjs`;
+
+CREATE TABLE `skq_skjs` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `PARENTID` int(11) NOT NULL COMMENT '申报汇总ID',
+  `SZBM` varchar(2) NOT NULL COMMENT '税种编码',
+  `SMBM` varchar(8) NOT NULL COMMENT '税目编码',
+  `JSJE` decimal(16,2) NOT NULL COMMENT '计税金额',
+  `SL` decimal(10,4) NOT NULL COMMENT '税率',
+  `YJSE` decimal(16,2) NOT NULL COMMENT '应计税额',
+  `CLFS` int(11) NOT NULL COMMENT '处理方式',
+  `CLBS` int(11) NOT NULL COMMENT '处理标示',
+  `SBSJ` datetime DEFAULT NULL COMMENT '申报时间',
+  `SBWDH` varchar(20) DEFAULT NULL COMMENT '申报文档号',
+  `PBBD` int(11) DEFAULT NULL COMMENT '票表比对',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_skjs` */
+
+/*Table structure for table `skq_wdhxh` */
+
+DROP TABLE IF EXISTS `skq_wdhxh`;
+
+CREATE TABLE `skq_wdhxh` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `WDHXH` int(11) NOT NULL COMMENT '文档号序号',
+  `LSXH` int(11) DEFAULT NULL,
+  `WJBMXH` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_wdhxh` */
+
+/*Table structure for table `skq_xebg` */
+
+DROP TABLE IF EXISTS `skq_xebg`;
+
+CREATE TABLE `skq_xebg` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NSRWJBM` varchar(20) NOT NULL COMMENT '纳税人微机编码',
+  `JQBH` varchar(16) NOT NULL COMMENT '机器编号',
+  `SYLJKPXE` decimal(16,2) NOT NULL COMMENT '剩余累计开票限额',
+  `SYLJTPXE` decimal(16,2) NOT NULL COMMENT '剩余累计退票限额',
+  `SQLJKPXE` decimal(16,2) NOT NULL COMMENT '申请累计开票限额',
+  `SQLJTPXE` decimal(16,2) NOT NULL COMMENT '申请累计退票限额',
+  `PZLJKPXE` decimal(16,2) DEFAULT NULL COMMENT '申请累计退票限额',
+  `PZLJTPXE` decimal(16,2) DEFAULT NULL COMMENT '批准累计退票限额',
+  `BGSM` varchar(200) DEFAULT NULL COMMENT '变更说明',
+  `SQSJ` datetime NOT NULL COMMENT '申请时间',
+  `CLBZ` int(11) NOT NULL COMMENT '处理标示',
+  `CLZ` varchar(20) DEFAULT NULL COMMENT '处理者',
+  `CLYJ` varchar(200) DEFAULT NULL COMMENT '处理意见',
+  `CLSJ` datetime DEFAULT NULL COMMENT '处理时间',
+  `XZBS` int(11) DEFAULT NULL COMMENT '下载标示',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `skq_xebg` */
 
 /*Table structure for table `skq_zclx` */
 
