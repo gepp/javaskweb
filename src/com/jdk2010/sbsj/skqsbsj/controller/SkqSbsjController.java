8f8cb688d1d916d32850473465441f8343ef9ed5
jdk2010 <jdk2010@163.com>
2015-12-17 20:01:22 +0800

    tj

--- src/com/jdk2010/sbsj/skqsbsj/controller/SkqSbsjController.java
+++ src/com/jdk2010/sbsj/skqsbsj/controller/SkqSbsjController.java
@@ -154,6 +154,268 @@ public class SkqSbsjController extends BaseController {
     }
     
     
+    @RequestMapping("/jdsbsjdk")
+    public String jdsbsjdk(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        String nsrwjbm = "";
+        String kpjzrq = "";
+        HashMap sbinfo = new HashMap();
+        DecimalFormat dg = new DecimalFormat("0.00");
+        HashMap cardInfo = (HashMap) session.getAttribute("UCARDINFO");
+        if (cardInfo == null || cardInfo.isEmpty()) {
+            request.setAttribute("errorMsg", "卡基本信息读取失败，请检查！");
+            return "/cxtj/error";
+        } else {
+            SkqNsrxx nsrxx = null;
+            HashMap kxx = (HashMap) cardInfo.get("EF02");
+            HashMap EF01 = (HashMap) cardInfo.get("EF01");
+            HashMap EF06 = (HashMap) cardInfo.get("EF06");
+            String jqbh = (String) EF06.get("JQBH");
+            String card_nsrwjbm = (String) kxx.get("NSRWJDM");
+            SkqJqxx jqxx = dalClient.queryForObject("select * from skq_jqxx where nsrwjbm='" + card_nsrwjbm
+                    + "' and jqbh='" + jqbh + "'", SkqJqxx.class);
+            if (jqxx == null) {
+                request.setAttribute("errorMsg", "纳税户不存在！用户卡中纳税户微机编码为：" + card_nsrwjbm + ",机器编号为：" + jqxx);
+                return "/cxtj/error";
+            } else {
+                nsrxx = skqNsrxxService.getNsrxxByNsrwjbm(card_nsrwjbm);
+                int status = nsrxx.getStatus();
+                if (status != 1) {
+                    request.setAttribute("errorMsg", "纳税户已注销！");
+                    return "/cxtj/error";
+                } else {
+                    sbinfo = (HashMap) cardInfo.get("EF04");
+                    if (sbinfo == null || sbinfo.isEmpty()) {
+                        request.setAttribute("errorMsg", "无申报，请先在机器上申报！");
+                        return "/cxtj/error";
+                    } else {
+                        String startdate = (String) sbinfo.get("startdate");
+                        String enddate = (String) sbinfo.get("enddate");
+
+                        startdate = startdate.substring(0, 4) + '-' + startdate.substring(4, 6) + '-'
+                                + startdate.substring(6, 8);
+                        enddate = enddate.substring(0, 4) + '-' + enddate.substring(4, 6) + '-'
+                                + enddate.substring(6, 8);
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        String sbsjsql = "select * from skq_sbsj " + " where SSKSSJ='" + startdate + "' and SSJZSJ='"
+                                + enddate + "' and JQBH='" + jqbh + "'";
+                        int size = dalClient.queryForObjectList(sbsjsql).size();
+                        if (size != 0) {
+                            request.setAttribute("errorMsg", "申报数据已存在，不能重复申报！");
+                            return "/cxtj/error";
+                        } else {
+                            // 保存申报数据
+                            double normalsum1 = Double.parseDouble((String) sbinfo.get("normalsum1"));
+                            normalsum1 = Double.parseDouble(dg.format(normalsum1 / 100));
+
+                            double normalsum2 = Double.parseDouble((String) sbinfo.get("normalsum2"));
+                            normalsum2 = Double.parseDouble(dg.format(normalsum2 / 100));
+
+                            double normalsum3 = Double.parseDouble((String) sbinfo.get("normalsum3"));
+                            normalsum3 = Double.parseDouble(dg.format(normalsum3 / 100));
+
+                            double normalsum4 = Double.parseDouble((String) sbinfo.get("normalsum4"));
+                            normalsum4 = Double.parseDouble(dg.format(normalsum4 / 100));
+
+                            double normalsum5 = Double.parseDouble((String) sbinfo.get("normalsum5"));
+                            normalsum5 = Double.parseDouble(dg.format(normalsum5 / 100));
+
+                            double normalsum6 = Double.parseDouble((String) sbinfo.get("normalsum6"));
+                            normalsum6 = Double.parseDouble(dg.format(normalsum6 / 100));
+
+                            double backsum1 = Double.parseDouble((String) sbinfo.get("backsum1"));
+                            backsum1 = Double.parseDouble(dg.format(backsum1 / 100));
+
+                            double backsum2 = Double.parseDouble((String) sbinfo.get("backsum2"));
+                            backsum2 = Double.parseDouble(dg.format(backsum2 / 100));
+
+                            double backsum3 = Double.parseDouble((String) sbinfo.get("backsum3"));
+                            backsum3 = Double.parseDouble(dg.format(backsum3 / 100));
+
+                            double backsum4 = Double.parseDouble((String) sbinfo.get("backsum4"));
+                            backsum4 = Double.parseDouble(dg.format(backsum4 / 100));
+
+                            double backsum5 = Double.parseDouble((String) sbinfo.get("backsum5"));
+                            backsum5 = Double.parseDouble(dg.format(backsum5 / 100));
+
+                            double backsum6 = Double.parseDouble((String) sbinfo.get("backsum6"));
+                            backsum6 = Double.parseDouble(dg.format(backsum6 / 100));
+
+                            double normalsum = Double.parseDouble(dg.format(normalsum1 + normalsum2 + normalsum3
+                                    + normalsum4 + normalsum5 + normalsum6));
+
+                            double backsum = Double.parseDouble(dg.format(backsum1 + backsum2 + backsum3 + backsum4
+                                    + backsum5 + backsum6));
+                            SkqSbsj sbsj = new SkqSbsj();
+                            sbsj.setNsrwjbm(nsrwjbm);
+                            sbsj.setJqbh(jqbh);
+                            sbsj.setZcpfs(Integer.parseInt((String) sbinfo.get("normalcnt")));
+                            sbsj.setTpfs(Integer.parseInt((String) sbinfo.get("backcnt")));
+                            sbsj.setFpfs(Integer.parseInt((String) sbinfo.get("deposecnt")));
+                            sbsj.setSskssj(DateUtil.parse(startdate));
+                            sbsj.setSsjzsj(DateUtil.parse(enddate));
+                            sbsj.setZcpzje(normalsum);
+                            sbsj.setTpzje(backsum);
+                            sbsj.setSbrq(new Date());
+                            sbsj.setSblx(2);
+                            int sid = skqSbsjService.save(sbsj);
+                            if (sid != 0) {
+                                ArrayList sbzbxx = new ArrayList();
+                                if (normalsum1 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index1"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", normalsum1);
+                                    hm.put("kplx", 1);
+                                    sbzbxx.add(hm);
+                                }
+                                if (normalsum2 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index2"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", normalsum2);
+                                    hm.put("kplx", 1);
+                                    sbzbxx.add(hm);
+                                }
+                                if (normalsum3 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index3"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", normalsum3);
+                                    hm.put("kplx", 1);
+                                    sbzbxx.add(hm);
+                                }
+                                if (normalsum4 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index4"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", normalsum4);
+                                    hm.put("kplx", 1);
+                                    sbzbxx.add(hm);
+                                }
+                                if (normalsum5 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index5"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", normalsum5);
+                                    hm.put("kplx", 1);
+                                    sbzbxx.add(hm);
+                                }
+                                if (normalsum6 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index6"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", normalsum6);
+                                    hm.put("kplx", 1);
+                                    sbzbxx.add(hm);
+                                }
+                                if (backsum1 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index1"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", backsum1 * -1);
+                                    hm.put("kplx", 2);
+
+                                    sbzbxx.add(hm);
+                                }
+                                if (backsum2 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index2"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", backsum2 * -1);
+                                    hm.put("kplx", 2);
+
+                                    sbzbxx.add(hm);
+                                }
+                                if (backsum3 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index3"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", backsum3 * -1);
+                                    hm.put("kplx", 2);
+                                    sbzbxx.add(hm);
+                                }
+                                if (backsum4 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index4"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", backsum4 * -1);
+                                    hm.put("kplx", 2);
+                                    sbzbxx.add(hm);
+                                }
+                                if (backsum5 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index5"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", backsum5 * -1);
+                                    hm.put("kplx", 2);
+                                    sbzbxx.add(hm);
+                                }
+                                if (backsum6 > 0) {
+                                    int smsy = Integer.parseInt((String) sbinfo.get("index6"));
+                                    String smbm = skqPmszService.getPmszBySmsy(smsy + "").getSmbm();
+                                    HashMap hm = new HashMap();
+                                    hm.put("parentid", sid);
+                                    hm.put("smbm", smbm);
+                                    hm.put("je", backsum6 * -1);
+                                    hm.put("kplx", 2);
+                                    sbzbxx.add(hm);
+                                }
+                                SkqSbsj skqSbsj = skqSbsjService.findById(sid, SkqSbsj.class);
+                                if (sbzbxx != null && !sbzbxx.isEmpty()) {
+                                    for(int i=0;i<sbzbxx.size();i++){
+                                        HashMap hm = (HashMap) sbzbxx.get(i);
+                                        String sql = "insert into SKQ_SBSJMX(PARENTID,SMBM,JE,KPLX) values("
+                                                + (Integer) hm.get("parentid")
+                                                + ",'"
+                                                + (String) hm.get("smbm")
+                                                + "',"
+                                                + (Double) hm.get("je")
+                                                + ","
+                                                + (Integer) hm.get("kplx") + ")";
+                                        dalClient.update(sql);
+                                    }
+                                }
+                                HashMap hmSb = new HashMap();
+                                hmSb.put("nsrxx", nsrxx);
+                                hmSb.put("sbsj", sbsj);
+                                request.setAttribute("hmSb", hmSb);
+                                return "/invoice/jdsb.sbxx";
+
+                            } else {
+                                request.setAttribute("errorMsg", "申报数据保存失败！");
+                                return "/cxtj/error";
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    
 
     @RequestMapping("/sbsjdk")
     public String sbsjdk(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -385,6 +647,22 @@ public class SkqSbsjController extends BaseController {
                                     sbzbxx.add(hm);
                                 }
                                 SkqSbsj skqSbsj = skqSbsjService.findById(sid, SkqSbsj.class);
+                                
+                                if (sbzbxx != null && !sbzbxx.isEmpty()) {
+                                    for(int i=0;i<sbzbxx.size();i++){
+                                        HashMap hm = (HashMap) sbzbxx.get(i);
+                                        String sql = "insert into SKQ_SBSJMX(PARENTID,SMBM,JE,KPLX) values("
+                                                + (Integer) hm.get("parentid")
+                                                + ",'"
+                                                + (String) hm.get("smbm")
+                                                + "',"
+                                                + (Double) hm.get("je")
+                                                + ","
+                                                + (Integer) hm.get("kplx") + ")";
+                                        dalClient.update(sql);
+                                    }
+                                }
+                                
                                 // 查询本月已申报机器数量
                                 String sql = "select count(SID) as num from SKQ_SBSJ where NSRWJBM='" + nsrwjbm
                                         + "' and SSKSSJ>='" + startdate + "' and SSJZSJ<='" + enddate + "' and SBLX=1";
