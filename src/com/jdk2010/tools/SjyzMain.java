e2f19b5d5f882eb28cbfb801219f433f210f8b2f
jdk2010 <jdk2010@163.com>
2015-12-25 10:18:14 +0800

    提交

--- src/com/jdk2010/tools/SjyzMain.java
+++ src/com/jdk2010/tools/SjyzMain.java
@@ -17,7 +17,7 @@ public class SjyzMain {
         BeanFactory factory = new ClassPathXmlApplicationContext("conf/spring/spring-dal.xml");
         DalClient mysqlDalClient=(DalClient)factory.getBean("dalClient");
         DalClient sybaseDalClient=(DalClient)factory.getBean("sybaseDalClient");
-        DalClient oracleDalClient=(DalClient)factory.getBean("oracleDalClient");
+//        DalClient oracleDalClient=(DalClient)factory.getBean("oracleDalClient");
 //        System.out.println("======移植t_hx_org_config======");
 //        List<THxOrgConfig> listOrgConfig=oracleDalClient.queryForObjectList("select * from t_hx_org_config",THxOrgConfig.class);
 //        for(int i=0;i<listOrgConfig.size();i++){
@@ -26,11 +26,11 @@ public class SjyzMain {
 //        System.out.println("======移植t_hx_org_config结束======");
         
         System.out.println("======移植skq_nsrxx======");
-        List<SkqNsrxx> nsrxxList=oracleDalClient.queryForObjectList("select * from skq_nsrxx",SkqNsrxx.class);
+        List<SkqNsrxx> nsrxxList=sybaseDalClient.queryForObjectList("select * from skq_nsrxx",SkqNsrxx.class);
         System.out.println("总共："+nsrxxList.size()+"条记录等待移植...");
         for(int i=0;i<nsrxxList.size();i++){
             System.out.println("剩余移植记录："+(nsrxxList.size()-i));
-            sybaseDalClient.save(nsrxxList.get(i));
+            mysqlDalClient.save(nsrxxList.get(i));
         }
         System.out.println("======移植skq_nsrxx结束======");
         
